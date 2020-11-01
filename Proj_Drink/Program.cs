bcd702c05abfd5520af4521654c1f3db809a2283
Chrystal Pinkney <chrystal.pinkney@gmail.com>
2020-10-31 20:14:31 -0500

    Cleaned up test comments

--- Proj_Drink/Program.cs
+++ Proj_Drink/Program.cs
@@ -7,13 +7,7 @@ namespace Proj_Drink
         /**/
         static void Main(string[] args)
         {
-            //var drinks = new List<Drink> //Test
-            //{
-            //    new Juice("Orange Juice", "not carbonated", "made from oranges."),
-            //    new Beer("Budweiser", "carbonated", 5.0),
-            //    new Soda("Pepsi", "carbonated")
-
-            //};
+           
             Drink[] drinks = new Drink[3]; 
             drinks[0] = new Juice("Orange Juice", "not carbonated", "made from oranges.");
             drinks[1] = new Beer("Budweiser", "carbonated", 5.0);
